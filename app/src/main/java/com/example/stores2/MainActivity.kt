7aeb95f20dc3ffbe82798097c610cf083c575787
Uriel <uriellopezaquino1.0@gmail.com>
2021-12-09 16:38:25 -0600

    Room----------------Entity

--- app/src/main/java/com/example/stores2/MainActivity.kt
+++ app/src/main/java/com/example/stores2/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.stores2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.stores2.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private  lateinit var mAdapter: StoreAdapter
+
+    private  lateinit var mGridLayout: GridLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.btnSave.setOnClickListener{
+            val store = Store(name = mBinding.etName.text.toString().trim())
+            mAdapter.add(store)
+        }
+
+        setupRecyclerView()
     }
+
+    private fun setupRecyclerView() {
+        mAdapter = StoreAdapter(mutableListOf(), this )
+        mGridLayout = GridLayoutManager(this, 2)
+
+        mBinding.recyclerView.apply{
+            setHasFixedSize(true)
+            layoutManager = mGridLayout
+            adapter = mAdapter
+        }
+    }
+
+    override fun onClick(store: Store){
+
+    }
+
 }
