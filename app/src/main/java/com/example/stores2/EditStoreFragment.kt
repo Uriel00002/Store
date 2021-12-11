b1ce0f4d5d986ae7db70d5b62dc2c3eab8871990
Uriel <uriellopezaquino1.0@gmail.com>
2021-12-11 17:12:24 -0600

    Barra de acciones

---
+++ app/src/main/java/com/example/stores2/EditStoreFragment.kt
@@ -0,0 +1,24 @@
+package com.example.stores2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.stores2.databinding.FragmentEditStoreBinding
+
+class EditStoreFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentEditStoreBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        mBinding = FragmentEditStoreBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+
+}
