4e209d094684a9cc39d4dac8a0b6ec2d6af2294d
Uriel <uriellopezaquino1.0@gmail.com>
2021-12-13 20:15:15 -0600

    Extension para clase String

--- app/src/main/java/com/example/stores2/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores2/EditStoreFragment.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.os.Bundle
 import android.view.*
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.cursosant.android.stores.databinding.FragmentEditStoreBinding
-import com.google.android.material.snackbar.Snackbar
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -30,6 +33,14 @@ class EditStoreFragment : Fragment() {
         mActivity?.supportActionBar?.title = getString(R.string.edit_store_title_add)
 
         setHasOptionsMenu(true)
+
+        mBinding.etPhotoUrl.addTextChangedListener {
+            Glide.with(this)
+                .load(mBinding.etPhotoUrl.text.toString())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .into(mBinding.imgPhoto)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -55,10 +66,7 @@ class EditStoreFragment : Fragment() {
 
                         hideKeyboard()
 
-                        Snackbar.make(mBinding.root,
-                            getString(R.string.edit_store_message_save_success),
-                            Snackbar.LENGTH_SHORT)
-                            .show()
+                        Toast.makeText(mActivity, R.string.edit_store_message_save_success, Toast.LENGTH_SHORT).show()
 
                         mActivity?.onBackPressed()
                     }
